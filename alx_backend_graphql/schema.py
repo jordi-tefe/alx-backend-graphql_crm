489e9a5b0c32c0804e0b899bcffd2b42cdeb2548
Yordanos Teferi <112561857+jordi-tefe@users.noreply.github.com>
2025-09-30 01:02:48 +0300

    Update schema.py

--- alx_backend_graphql/schema.py
+++ alx_backend_graphql/schema.py
@@ -8,3 +8,24 @@ class Mutation(CRMMutation, graphene.ObjectType):
     pass
 
 schema = graphene.Schema(query=Query, mutation=Mutation)
+import graphene
+from crm.models import Product
+
+class UpdateLowStockProducts(graphene.Mutation):
+    class Arguments:
+        pass
+
+    success = graphene.Boolean()
+    updated_products = graphene.List(graphene.String)
+
+    def mutate(root, info):
+        low_stock = Product.objects.filter(stock__lt=10)
+        updated_names = []
+        for p in low_stock:
+            p.stock += 10
+            p.save()
+            updated_names.append(f"{p.name} ({p.stock})")
+        return UpdateLowStockProducts(success=True, updated_products=updated_names)
+
+class Mutation(graphene.ObjectType):
+    update_low_stock_products = UpdateLowStockProducts.Field()
